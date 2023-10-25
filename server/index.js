7f01b79f0bcc4671fe26f2b4fe8e8f080f10831d
FlagerCarft <laramnbvo123@gmail.com>
2023-10-25 16:10:26 -0500

    Actualizacion Chat

--- server/index.js
+++ server/index.js
@@ -4,28 +4,33 @@ import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 
+import bodyParser from 'body-parser'
+import webrtc from 'webrtc'
+
 const port = process.env.PORT ?? 3000
 
-const app = express()
-const server = createServer(app)
+const app = express();
+const server = createServer(app);
 const io = new Server(server, {
     connectionStateRecovery: {}
-})
+});
+
+//CONST PARA EL STREAM
 
 io.on('connection', (socket) => {
-    console.log('Un usuario se ha conectado')
+    console.log('Un usuario se ha conectado');
 
     socket.on('disconnect', () => {
         console.log('Un usuario se ha desconectado')
-    })
+    });
 
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg)
-    })
-})
+    });
+});
 
 
-app.use(logger('dev'))
+app.use(logger('dev'));
 
 app.use(express.static('public'));
 
@@ -35,4 +40,55 @@ app.get('/', (req, res) => {
 
 server.listen(port, () => {
     console.log('Server running on port ' + port)
-})
+});
+
+
+//PARA EL STREAM
+let senderStream;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/consumer", async ({ body }, res) => {
+    const peer = new webrtc.RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+    const desc = new webrtc.RTCSessionDescription(body.sdp);
+    await peer.setRemoteDescription(desc);
+    senderStream.getTracks().forEach(track => peer.addTrack(track, senderStream));
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+    const payload = {
+        sdp: peer.localDescription
+    }
+
+    res.json(payload);
+});
+
+app.post('/broadcast', async ({ body }, res) => {
+    const peer = new webrtc.RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+    peer.ontrack = (e) => handleTrackEvent(e, peer);
+    const desc = new webrtc.RTCSessionDescription(body.sdp);
+    await peer.setRemoteDescription(desc);
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+    const payload = {
+        sdp: peer.localDescription
+    }
+
+    res.json(payload);
+});
+
+function handleTrackEvent(e, peer) {
+    senderStream = e.streams[0];
+};
