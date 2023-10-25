b886cd176a56451db5b2cd397f043235b2cd7937
FlagerCarft <laramnbvo123@gmail.com>
2023-10-25 11:25:41 -0500

    Chat sin estilo

--- public/style/js/script_socket.js
+++ public/style/js/script_socket.js
@@ -1,46 +1,27 @@
 import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
 
-    const getUsername = async () => {
-      const username = localStorage.getItem('username')
-      if (username) {
-        console.log(`User existed ${username}`)
-        return username
-      }
+const socket = io()
 
-      const res = await fetch('https://random-data-api.com/api/users/random_user')
-      const { username: randomUsername } = await res.json()
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const messages = document.getElementById('messages')
 
-      localStorage.setItem('username', randomUsername)
-      return randomUsername
-    }
+socket.on('chat message', (msg, serverOffset, username) => {
+  const item = `<li>
+    <p>${msg}</p>
+    <small>${username}</small>
+  </li>`
+  messages.insertAdjacentHTML('beforeend', item)
+  socket.auth.serverOffset = serverOffset
+  // scroll to bottom of messages
+  messages.scrollTop = messages.scrollHeight
+})
 
-    const socket = io({
-      auth: {
-        username: await getUsername(),
-        serverOffset: 0
-      }
-    })
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-    const form = document.getElementById('form')
-    const input = document.getElementById('input')
-    const messages = document.getElementById('messages')
-
-    socket.on('chat message', (msg, serverOffset, username) => {
-      const item = `<li>
-        <p>${msg}</p>
-        <small>${username}</small>
-      </li>`
-      messages.insertAdjacentHTML('beforeend', item)
-      socket.auth.serverOffset = serverOffset
-      // scroll to bottom of messages
-      messages.scrollTop = messages.scrollHeight
-    })
-
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-
-      if (input.value) {
-        socket.emit('chat message', input.value)
-        input.value = ''
-      }
-    })
+  if (input.value) {
+    socket.emit('chat message', input.value)
+    input.value = ''
+  }
+})
--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+    connectionStateRecovery: {}
+})
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado')
+
+    socket.on('disconnect', () => {
+        console.log('Un usuario se ha desconectado')
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+
+
 app.use(logger('dev'))
 
 app.use(express.static('public'));
@@ -12,6 +33,6 @@ app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server running on port ' + port)
 })
